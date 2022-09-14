cfb7ef7af57e883d67ccf9bdf4517667f18d694e
agustin <agustincaceres97@gmail.com>
2022-09-13 21:49:39 -0300

    Proyecto de pasteleria desde mobile first

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+
+let btnOk= true;
+
+const btnMenu= document.querySelector('.btn-menu').addEventListener('click',function Menu(){
+    
+    let MenuResponsive = document.querySelector('.ul-nav');
+    if (btnOk == true) {
+        MenuResponsive.style.transform= 'translateX(0%)';
+        MenuResponsive.style.transitionProperty= 'transform';
+        MenuResponsive.style.transitionDuration= '2s';
+        MenuResponsive.style.zIndex='50'
+        btnOk= false;
+        console.log(btnOk)
+    } else {
+        MenuResponsive.style.transform= 'translateX(100%)';
+        MenuResponsive.style.transitionProperty= 'transform';
+        MenuResponsive.style.transitionDuration= '2s';
+        MenuResponsive.style.zIndex='50'
+        btnOk= true;
+        console.log(btnOk)
+    }
+}
+)
