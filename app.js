374087411865c064138370f7ca1e4c9e7f9819f3
agustin <agustincaceres97@gmail.com>
2022-09-16 11:09:05 -0300

    hice mas rapido la entrada y salida del menu desplegable

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const btnMenu= document.querySelector('.btn-menu').addEventListener('click',func
     if (btnOk == true) {
         MenuResponsive.style.transform= 'translateX(0%)';
         MenuResponsive.style.transitionProperty= 'transform';
-        MenuResponsive.style.transitionDuration= '2s';
+        MenuResponsive.style.transitionDuration= '1s';
         MenuResponsive.style.zIndex='50'
         btnOk= false;
         console.log(btnOk)
     } else {
         MenuResponsive.style.transform= 'translateX(100%)';
         MenuResponsive.style.transitionProperty= 'transform';
-        MenuResponsive.style.transitionDuration= '2s';
+        MenuResponsive.style.transitionDuration= '1s';
         MenuResponsive.style.zIndex='50'
         btnOk= true;
         console.log(btnOk)
